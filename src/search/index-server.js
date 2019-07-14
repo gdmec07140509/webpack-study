fbe91c0c8bf0c6cda40801bb589a8641ead4fabf
CT <821982911@qq.com>
2019-07-15 00:29:24 +0800

    ssr step 1

---
+++ src/search/index-server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// import React from 'react';
+// import logo from './images/1.png';
+// import './search.less';
+// import '../../common'
+
+const React = require('react');
+const logo = require('./images/1.png');
+require('./search.less');
+require('../../common');
+
+class Search extends React.Component {
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      Text: null
+    }
+  }
+
+  loadComponent() {
+    import ('./text.js').then((Text) =>  {
+        this.setState({
+          Text: Text.default
+        })
+      })
+  }
+
+  render() {
+    const { Text } = this.state;
+    return <div className="search-text">
+      {
+        Text ? <Text /> : null
+      }
+      <img src={logo}  onClick={this.loadComponent.bind(this)}/>
+    </div>;
+  }
+}
+
+module.exports = <Search />;
